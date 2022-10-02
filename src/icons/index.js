7a264598015732e77b0899b36304dcb9f8794e13
ylf <yanlufei0209@163.com>
2022-10-02 16:52:40 +0800

    1.0.2

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,6 +4,7 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
+/* 引入svg文件 */
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
